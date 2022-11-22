3b941baf556058cf35039cc22d91226c2fc17e7c
Your Name <you@example.com>
2022-11-22 21:23:22 +0400

    1zadanie

---
+++ seminar1.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class seminar1 {
+    public static void main(String[] args){
+        int  k = 2001;        
+        int i = new Random().nextInt(k);
+        i=i-1000;
+            System.out.print(i);
+    }
+}
